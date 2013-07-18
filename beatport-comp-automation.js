f27834f67c159af4c56d699e4631dca605f3b284
Fry Falavel <fry@justgomusic.com>
2013-07-18 16:58:43 +0100

    update texts

--- beatport-comp-automation.js
+++ beatport-comp-automation.js
@@ -42,8 +42,8 @@
 (function () {
 
 	/* VARIABLES TO CHANGE */
-	var url_to_share = "http://btprt.dj/196XNJc";
-	var name_of_track = "Fedde Le Grand";
+	var url_to_share = "http://btprt.dj/12uOPOZ";
+	var name_of_track = "Nicky Romero";
 
 
 	/* DO NOT TOUCH BELOW (unless you're a geek) */
@@ -52,22 +52,25 @@
 
 	var artist = $(".span6").children(".padBox").children(".txtxx.txtWhite").children("a").text();
 
-	var greetings = new Array("Wow ", "This is awesome ", "That's dope ", "Love this ", "Great track ", "Nice track ", "Love the way to made this ", "You have good skills ", "This is a really cool track ", "This track has something original. Nice work ", "Oh waow... this is good "),
-	greeting = greetings[Math.floor( Math.random() * greetings.length )];
+	var greetings = new Array("Wow ", "This is awesome ", "That's dope ", "Love this track ", "Great track ", "Nice track ", "Love the way to made this ", "Dope track ", "Cool track ", "Nice work ", "This is good "),
+	var greeting = greetings[Math.floor( Math.random() * greetings.length )];
 
-	var invites = new Array("! Have a listen to my "+name_of_track+" track if you have 1 minute, I would love to hear your feedback. If you think I did well, please vote back ","! Listen to my "+name_of_track+" track if you have the time, I'd like to read your feedback and maybe vote back? ", "! Would you mind gimme some feedback on my "+name_of_track+" track? and maybe vote back? Check it out here: ", ". Please have a listen to my "+name_of_track+" track. Feel free to vote back: ", "! What do you think of my "+name_of_track+" track? vote back? ", "!!! I would be grateful if you give me your feedback on mine (and I would love you if you vote), check it out here: "),
-	invite = invites[Math.floor( Math.random() * invites.length )];
+	//var invites = new Array("! Have a listen to my "+name_of_track+" track if you have 1 minute, I would love to hear your feedback. If you think I did well, please vote back ","! Listen to my "+name_of_track+" track if you have the time, I'd like to read your feedback and maybe vote back? ", "! Would you mind gimme some feedback on my "+name_of_track+" track? and maybe vote back? Check it out here: ", ". Please have a listen to my "+name_of_track+" track. Feel free to vote back: ", "! What do you think of my "+name_of_track+" track? vote back? ", "!!! I would be grateful if you give me your feedback on mine (and I would love you if you vote), check it out here: "),
+	//var invite = invites[Math.floor( Math.random() * invites.length )];
+	var invite = ". Vote back?";
 
-	var votes = new Array("! you got my vote #", ". Here my vote #", ". You deserve this vote #", "! You totally get my vote number ", "! Just added my vote :-) Now you have ", ". Just voted for you, now you have ", ". VOTED #", ". HERE MY VOTE #"),
+	/*var votes = new Array("! you got my vote #", ". Here my vote #", ". You deserve this vote #", "! You totally get my vote number ", "! Just added my vote :-) Now you have ", ". Just voted for you, now you have ", ". VOTED #", ". HERE MY VOTE #"),
 	vote = votes[Math.floor( Math.random() * votes.length )];
+	*/
+	var vote = "VOTED #";
 
-	var no_votes = new Array("! I will come back to vote for you when the vote will start", ". Definitely coming back in few days to make my vote for you", ". Hope you will win something with this", "! You deserve to win something with this, I'll come back to vote", "! I wish I could vote now, I'll come back to vote", ". I'll come back when the votes start to support you with my vote", ". For sure I will come back to vote for this", ". This Definitely catched my attention, I'll come back to vote for your track"),
-	no_vote = no_votes[Math.floor( Math.random() * no_votes.length )];
+	//var no_votes = new Array("! I will come back to vote for you when the vote will start", ". Definitely coming back in few days to make my vote for you", ". Hope you will win something with this", "! You deserve to win something with this, I'll come back to vote", "! I wish I could vote now, I'll come back to vote", ". I'll come back when the votes start to support you with my vote", ". For sure I will come back to vote for this", ". This Definitely catched my attention, I'll come back to vote for your track"),
+	//var no_vote = no_votes[Math.floor( Math.random() * no_votes.length )];
 
-	var ends = new Array(" Find me on soundcloud if you want to get in touch.", " Get in touch on soundcloud.", " Poke me on soundcloud.", " See you on soundcloud?", " See you.", " Good luck.", " Wish you luck.", " Have a good day.", " Bye.", " Peace.", " Hope you win something with this", " See you around"),
-	end = ends[Math.floor( Math.random() * ends.length )];
+	//var ends = new Array(" Find me on soundcloud if you want to get in touch.", " Get in touch on soundcloud.", " Poke me on soundcloud.", " See you on soundcloud?", " See you.", " Good luck.", " Wish you luck.", " Have a good day.", " Bye.", " Peace.", " Hope you win something with this", " See you around"),
+	//var end = ends[Math.floor( Math.random() * ends.length )];
 
-	var message = greeting + artist + vote + number_of_votes + invite + "\n\r\n\r" + url_to_share + "\n\r\n\r" + end;
+	var message = greeting + artist + vote + number_of_votes + invite + "\n\r\n\r" + url_to_share; // + "\n\r\n\r" + end;
 
 	setTimeout(function () { $("#comment").val(message); }, 500); 
 	setTimeout(function () { $(".vote").children(".button").click(); }, 700); 
